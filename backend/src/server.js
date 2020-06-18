557dfc84adf5e0565283c3b1ab34227891ef0430
aabastos <andrebastos1198@gmail.com>
2020-06-18 03:47:21 -0300

    db created and sessions (users) route created

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,19 @@
 const express = require("express");
+const routes = require("./routes");
+const mongoose = require("mongoose");
 
 const app = express();
+app.use(express.json());
+app.use(routes);
+
+mongoose.connect(
+  "mongodb+srv://db_admin:@dministrator@aircnc-9mgwh.mongodb.net/aircnc?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 app.get("/", function (req, res) {
   res.json({ message: "Hello World" });
 });
-
-app.use(express.json());
 
 app.listen(3333, function () {
   console.log("Servidor aberto na porta 3333.");
