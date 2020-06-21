f4846553e63062ae30d3bddebf79ea6c6d0d3742
aabastos <andrebastos1198@gmail.com>
2020-06-21 00:31:23 -0300

    application supporting login (without route yet)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import api from "./services/api";
 import "./App.css";
 
 import logo from "./assets/logo.svg";
 
 function App() {
+  const [email, setEmail] = useState("");
+
+  function handleInputChange(event) {
+    const { value } = event.target;
+    setEmail(value);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.post("/sessions", { email: email });
+    localStorage.setItem("user", response.data._id);
+
+    setEmail("");
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnC" />
@@ -15,12 +31,15 @@ function App() {
           <strong>talentos</strong> para sua empresa{" "}
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="emal">E-MAIL *</label>
           <input
+            onChange={handleInputChange}
             type="email"
             id="email"
+            name="email"
             placeholder="Digite aqui o seu e-mail"
+            value={email}
           />
           <button className="btn" type="submit">
             Entrar
