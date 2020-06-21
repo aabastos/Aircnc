1f4888b6d82d713bfcaa932d968b70da64d00f04
aabastos <andrebastos1198@gmail.com>
2020-06-21 00:09:53 -0300

    css login page finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,34 @@
 import React from "react";
+import api from "./services/api";
 import "./App.css";
 
+import logo from "./assets/logo.svg";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container">
+      <img src={logo} alt="AirCnC" />
+
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre{" "}
+          <strong>talentos</strong> para sua empresa{" "}
+        </p>
+
+        <form>
+          <label htmlFor="emal">E-MAIL *</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Digite aqui o seu e-mail"
+          />
+          <button className="btn" type="submit">
+            Entrar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
