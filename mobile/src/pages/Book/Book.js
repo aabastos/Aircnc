c1a15bd6794c29ea001d0d16b3b0d749077c4bb4
Andre Bastos <andrebastos1198@gmail.com>
2020-06-27 23:45:04 -0300

    booking funcionality finished

--- mobile/src/pages/Book/Book.js
+++ mobile/src/pages/Book/Book.js
@@ -1,9 +1,38 @@
-import React from "react";
-import { SafeAreaView, Text, TextInput, TouchableOpacity } from "react-native";
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, Text, TextInput, TouchableOpacity, Alert, AsyncStorage } from "react-native";
+
+import api from "../../services/api";
 
 import styles from "./styles";
 
 export default function Book({ navigation }) {
+  const [userId, setUserId] = useState("");
+  const [bookDate, setBookDate] = useState("");
+
+  useEffect(() => {
+    async function loadUser() {
+      const userId = await AsyncStorage.getItem("user");
+
+      setUserId(userId);
+    }
+
+    loadUser();
+  }, [])
+
+  async function handleConfirmButton() {
+    const user = userId;
+    const spot = navigation.getParam("id");
+
+    const response = await api.post(`/spots/${spot}/booking`, { date: bookDate }, {
+      headers: {
+        user_id: user
+      }
+    });
+
+    Alert.alert("Solicitação de reserva enviada!");
+
+    navigation.navigate("List");
+  }
 
   function handleCancelButton() {
     navigation.navigate("List");
@@ -17,11 +46,13 @@ export default function Book({ navigation }) {
         style={styles.input}
         placeholder="Qual data você quer reservar? (dd/mm/yyyy)"
         placeholderTextColor="#999"
-        keyboardType="numeric"
+        keyboardType="default"
         autoCorrect={false}
+        onChangeText={(text) => setBookDate(text)}
+        value={bookDate}
       />
 
-      <TouchableOpacity style={styles.confirmButton}>
+      <TouchableOpacity style={styles.confirmButton} onPress={handleConfirmButton}>
         <Text style={styles.buttonText}>Solicitar Reserva</Text>
       </TouchableOpacity>
 
